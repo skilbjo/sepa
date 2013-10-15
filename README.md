3227db692555425d187618294e82bec83e8d6587
skilbjo <john.skilbeck@gmail.com>
2013-10-15 15:40:41 -0700

    finished

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-pdf-generator-using-ruby
+sepa-pdf-&-html-generator
 ========================
 
-Generates PDF via erb / html and wkhtmltopdf from a CSV. Written in Ruby.
+Generates PDF via erb / html and wkhtmltopdf from a CSV. Written in Ruby by John Skilbeck.
 
 Introduction
 ===
@@ -54,29 +54,48 @@ Customize the output directories with this little bit:
 
 Your CSV is file is titled csv-example.csv - feel free to change the headers and title! Just don't forget, if you change the file name, update
 
-	contents = CSV.open 'csv-example.csv', headers: true, header_converters: :symbol
+	contents = CSV.open 'input/input.csv', headers: true, header_converters: :symbol
 
 
 And if you change the headers, update the header variables
 
 	id=row[:id]
-	first_name = row[:first_name]
-	last_name = row[:last_name]
-	address1 = row[:address1]
-	address2 = row[:address2]
-	address3 = row[:address3]
-	balance = row[:balance]
+	firstname = row[:firstname]
+	lastname = row[:lastname]
+	applicant_email = row[:applicant_email]
+	listing_name = row[:listing_name]
+	address = row[:address]
+	city = row[:city]
+	state = row[:state]
+	postalcode = row[:postalcode]
+	country = row[:country]
+	mandate_old = row[:mandate_old]
+	mandate_new = row[:mandate_new]
+	bic = row[:biccode]
 
 
 Setting the erb variables in form-letter.rb like so:
 	
-	amount = balance
-	name = first_name
+	firstname_erb = firstname
+	lastname_erb = lastname
+	listing_name_erb = listing_name
+	address_erb = address
+	city_erb = city
+	state_erb = state
+	postalcode_erb = postalcode
+	country_erb = country
+	mandate_old_erb = mandate_old
+	mandate_new_erb = mandate_new
+	bic_erb = bic
+	mandate_date_erb = mandate_date
 
 
 and in form-letter.erb like this
 
-	<h3>Dear <%= first_name %>,</h3>
-	<p>Your adjustment amount is $<%= amount %>.</p>
+	<h4>Customer Name: <%= firstname_erb %> <%= lastname_erb %></h4>
+    <tr>
+      <td>Bank Identification Code (BIC): </td>
+      <td><%= bic_erb %></td>
+    </tr>
 
 Enjoy!
--- sepa.rb
+++ sepa.rb
@@ -7,7 +7,7 @@ PDFKit.configure do |config|
 end
 
 def save_letters(id,form_letter)
-	Dir.mkdir('output/html') unless Dir.exists? 'output'
+	Dir.mkdir('output/html') unless Dir.exists? 'output/html'
 	Dir.mkdir('output/pdf') unless Dir.exists? 'output/pdf'
 	filename = "output/html/letter_#{id}.html"
 
@@ -19,7 +19,6 @@ def save_letters(id,form_letter)
 	# generate PDF
 	kit = PDFKit.new(form_letter)
 	file = kit.to_file("output/pdf/letter_#{id}.pdf")
-
 end
 
 # read ERB file (ERB contains HTML code)
@@ -28,25 +27,44 @@ template_letter = File.read 'sepa.erb'
 erb_template = ERB.new template_letter
 
 # open and parse csv
-contents = CSV.open 'csv-example.csv', headers: true, header_converters: :symbol
+contents = CSV.open 'input/input.csv', headers: true, header_converters: :symbol
 
 contents.each do |row|
-	# set data variables using headers in CSV
-	id=row[:id]
-	first_name = row[:first_name]
-	last_name = row[:last_name]
-	address1 = row[:address1]
-	address2 = row[:address2]
-	address3 = row[:address3]
-	balance = row[:balance]
 
-	# set erb variables
-	amount = balance
-	name = first_name
+		# set data variables using headers in CSV
+		id=row[:id]
+		firstname = row[:firstname]
+		lastname = row[:lastname]
+		applicant_email = row[:applicant_email]
+		listing_name = row[:listing_name]
+		address = row[:address]
+		city = row[:city]
+		state = row[:state]
+		postalcode = row[:postalcode]
+		country = row[:country]
+		mandate_old = row[:mandate_old]
+		mandate_new = row[:mandate_new]
+		mandate_date = row[:mandate_date]
+		bic = row[:biccode]
 
-	# fill out erb template
-	form_letter = erb_template.result(binding)
-	save_letters(id,form_letter)
+		# set erb variables
+		firstname_erb = firstname
+		lastname_erb = lastname
+		listing_name_erb = listing_name
+		address_erb = address
+		city_erb = city
+		state_erb = state
+		postalcode_erb = postalcode
+		country_erb = country
+		mandate_old_erb = mandate_old
+		mandate_new_erb = mandate_new
+		bic_erb = bic
+		mandate_date_erb = mandate_date
+
+
+		# fill out erb template
+		form_letter = erb_template.result(binding)
+		save_letters(id,form_letter)
 end
 
-puts 'Letters initialized.'
+puts 'Letters completed.'
